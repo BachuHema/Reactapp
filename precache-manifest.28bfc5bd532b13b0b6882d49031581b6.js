7e92f72dce460a96879a16a2832b252b050335e1
Hema <bachuhema@gmail.com>
2020-07-15 15:38:21 +0530

    Updates

---
+++ precache-manifest.28bfc5bd532b13b0b6882d49031581b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b04d9af5bec975e1ef3b9ece8ce4e4",
+    "url": "/Reactapp/index.html"
+  },
+  {
+    "revision": "af861afbf2d08fa95f31",
+    "url": "/Reactapp/static/css/main.e55cd904.chunk.css"
+  },
+  {
+    "revision": "17f94e910f4759926f8a",
+    "url": "/Reactapp/static/js/2.39b141dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactapp/static/js/2.39b141dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af861afbf2d08fa95f31",
+    "url": "/Reactapp/static/js/main.48c324d7.chunk.js"
+  },
+  {
+    "revision": "6e2d2906c0a65da14c01",
+    "url": "/Reactapp/static/js/runtime-main.50c7724a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
